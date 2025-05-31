a44ca20e4188660dfbb68e7a7bd3b6af9db279db
ElioRodrigues <neto.elio@hotmail.com>
2025-05-31 07:50:56 -0300

    alterações e novas telas

--- src/main/java/com/example/ProjetoAssistenciaJuridica/Config/SecurityConfig.java
+++ src/main/java/com/example/ProjetoAssistenciaJuridica/Config/SecurityConfig.java
@@ -1,17 +1,22 @@
 package com.example.ProjetoAssistenciaJuridica.Config;
 
+import org.springframework.beans.factory.annotation.Autowired; // <-- ADICIONAR SE NÃO EXISTIR
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity; // <-- ADICIONAR
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler; // <-- ADICIONAR SE NÃO EXISTIR
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 
 @Configuration
+@EnableMethodSecurity
 public class SecurityConfig {
-
+    @Autowired // <-- ADICIONAR ESTA LINHA (injeta o handler criado no Passo 1)
+    private AuthenticationSuccessHandler customAuthenticationSuccessHandler;
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http ) throws Exception {
         http
@@ -29,7 +34,8 @@ public class SecurityConfig {
                         .loginProcessingUrl("/login")
                         .usernameParameter("email")
                         .passwordParameter("senha")
-                        .defaultSuccessUrl("/", true)
+                        //.defaultSuccessUrl("/", true)
+                        .successHandler(customAuthenticationSuccessHandler) // <-- ADICIONAR ESTA LINHA (usa o handler)
                         .permitAll()
                 )
                 .logout(logout -> logout
